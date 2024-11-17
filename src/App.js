fc31be593fafaf021961c3b5fc28370aa4997c03
dkpunit <dkpunit@oultook.com>
2024-11-17 18:58:29 -0500

    Update: Removed outer white space from the dashboard homepage, updated CSS styles for full screen alignment and consistent layout design.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './routes/Dashboard';
-import Logs from './routes/Logs';
+import './App.css';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/logs" element={<Logs />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <Dashboard />
+    </div>
   );
 }
 
