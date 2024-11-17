7821e30c9e8d1ce191d0fac4725cb83565da1521
dkpunit <dkpunit@oultook.com>
2024-11-17 01:06:38 -0500

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./Dashboard";
+import Dashboard from './routes/Dashboard';
 import Logs from "./Logs";
 
 function App() {
