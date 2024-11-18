b6477fc1bc64b6388d7953923e6b9784156ebcd4
dkpunit <dkpunit@oultook.com>
2024-11-18 00:13:36 -0500

    fixed directory name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
-import LogsPage from "src\pages\LogPages.js";
-import GoalsPage from "src\pages\GoalPages.js";
+import LogsPage from "src\pages\LogsPages.js";
+import GoalsPage from "src\pages\GoalsPages.js";
 
 function App() {
   return (
