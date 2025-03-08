61468c79e38a6bf37a238abeaae0aaf6aba90051
dkpunit <dkpunit@oultook.com>
2025-03-08 10:55:17 -0500

    updated code

--- src/context/WorkoutContext.js
+++ src/context/WorkoutContext.js
@@ -4,9 +4,11 @@ import { createContext, useState } from 'react';
 export const WorkoutContext = createContext();
 
 export const WorkoutProvider = ({ children }) => {
-  const [workouts, setWorkouts] = useState([]);
+  const [workouts, setWorkouts] = useState([]); // Ensure it's empty initially
 
-  const addWorkout = workout => setWorkouts([...workouts, workout]);
+  const addWorkout = (workout) => {
+    setWorkouts([...workouts, workout]);
+  };
 
   return (
     <WorkoutContext.Provider value={{ workouts, addWorkout }}>
