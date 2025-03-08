81ea689c8e968b30ab363498399a99bf50171c86
dkpunit <dkpunit@oultook.com>
2025-03-08 10:02:30 -0500

    updated codeq

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
 import Dashboard from './routes/Dashboard';
 import Logs from './routes/Logs';
 import GoalsPage from './pages/GoalsPage';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <Sidebar />
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/logs" element={<Logs />} />
-          <Route path="/goals" element={<GoalsPage />} />
-        </Routes>
+      <div className="content-wrapper">
+        <Sidebar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/logs" element={<Logs />} />
+            <Route path="/goals" element={<GoalsPage />} />
+          </Routes>   
+        </div>
       </div>
     </Router>
   );
