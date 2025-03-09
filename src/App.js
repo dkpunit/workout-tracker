88a2e958fa4c91c0b059064f5b4df2b37848e5b3
dkpunit <dkpunit@oultook.com>
2025-03-09 15:39:02 -0400

    Added /about-tracker route for Learn More page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Welcome from "./pages/Welcome";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import WorkoutLogs from "./pages/WorkoutLogs";
+import AboutTracker from "./pages/AboutTracker"; // Import new About Tracker page
 import Sidebar from "./components/Sidebar";
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
             <Route path="/login" element={!user ? <Login /> : <Navigate to="/dashboard" />} />
             <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
             <Route path="/logs" element={user ? <WorkoutLogs /> : <Navigate to="/login" />} />
+            <Route path="/about-tracker" element={<AboutTracker />} /> {/* New route for Learn More page */}
           </Routes>
         </div>
       </div>
